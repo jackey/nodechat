9f640530de118b165024bc4003713dc318d167f8
JackeyChan <jackey@wiredcraft.com>
2012-04-09 11:25:31 +0800

    Add new command:status.

--- commands.js
+++ commands.js
@@ -12,23 +12,26 @@ module.exports = function (clients) {
       var uid = match[1];
       var msg = match[2];
       if (isNaN(parseInt(uid))) {
-        socket.write('Missed uid.');
+        socket.write('missed uid');
       }
       else {
         msg || (msg = '');
         var registered = false;
         clients.forEach(function (client) {
           if (client.uid == uid) {
-            client.write('user:'+client.nickname + ' msg:' + msg);
-            socket.write('Sent message');
-            registered = true;
+							console.log(client);
+							console.log(socket);
+							client.write(msg);
+							// client.write(socket.uid + ' / '+ socket.nickname +' wrote :' + msg);
+							// socket.write('Sent message');
+							registered = true;
           }
         });
         if (!registered) socket.write('user with ' + uid + ' not found');
       }
     }
     else {
-      throw new Error('Miss uid or msg argment.');
+      throw new Error('miss uid or msg argument');
     }
   };
 
@@ -42,7 +45,7 @@ module.exports = function (clients) {
       var uid = match[1];
       var nickname = match[2];
       if (isNaN(parseInt(uid))) {
-        socket.write('Missed uid.');
+        socket.write('missed uid');
       }
       else {
         nickname || (nickname = '');
@@ -54,15 +57,15 @@ module.exports = function (clients) {
           socket.uid = uid;
           socket.nickname = nickname;
           clients.push(socket);
-          socket.write('Registe successed.');
+					//  socket.write('Registration successed.');
         }
         else {
-          socket.write('User with ' + uid + ' is be registed.');
+          socket.write('user ' + uid + ' is already registered');
         }
       }
     }
     else {
-      throw new Error('Miss uid or msg argment.');
+      throw new Error('miss uid or msg argment');
     }
   };
   
@@ -76,20 +79,45 @@ module.exports = function (clients) {
       if (!isNaN(parseInt(uid))) {
         clients.forEach(function (client) {
           if (client.uid == uid) {
-            socket.write('Quit successed.');
+            socket.write('quit successed');
             client.destroy();
             delete client;
           }
         });
       }
       else {
-        socket.write('Missed uid.');
+        socket.write('missed uid');
       }
     }
     
   };
   
-
+  // Status of client.
+  // For now available status are [writing, writed]
+  // Note: status is from sender.
+  // ex, /status uid writing
+  commands['status'] = function (socket, msg) {
+      var spliter = /^(\S+)\s*(.*)$/;
+      var match = msg.match(spliter);
+      var uid = match[1];
+      var status = match[2];
+      // Check params.
+      if (!uid || typeof uid == 'undefined' ) || isNaN(parseInt(uid))) {
+          socket.write('missed uid');
+          return;
+      }
+      if (!status || typeof status == 'undefined') {
+          socket.write('missed status');
+          return;
+      }
+      clients.forEach(function (client) {
+          if (client.uid == uid) {
+              // Here we go.
+              // Just send status to client.
+              socket.write(status);
+          }
+      });
+  }
   console.log(clients);
   return commands;
 }
